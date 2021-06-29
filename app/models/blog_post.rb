acc7eede0faa449c88b990da2fda122cf7d109c4
MDAM182 <mdam2345@gmail.com>
2021-06-29 00:02:47 -0400

    validations/attributes  added to BlogPost model

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,3 +1,6 @@
 class BlogPost < ApplicationRecord
-
+   validates :title, presence: true
+   validates :content, presence: true
+   validates :content, uniqueness: true
+   validates :title, uniqueness: true
 end
