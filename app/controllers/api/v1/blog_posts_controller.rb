e0bbae2038fa0b7831bef33f19420432aead2b49
MDAM182 <mdam2345@gmail.com>
2021-07-03 15:04:04 -0400

    modified controller

--- app/controllers/api/v1/blog_posts_controller.rb
+++ app/controllers/api/v1/blog_posts_controller.rb
@@ -2,48 +2,47 @@ class Api::V1::BlogPostsController < ApplicationController
   def index
     @blog_posts = BlogPost.all
     render json: @blog_posts
-    # render json: BlogPostSerializer.new(@blog_posts)
-  end]
+    # render json: BlogPostSerializer.new(blog_posts)
+  end
 
-  def create
-    @blog_post = BlogPost.new(blog_post_params)
-    if @blog_post.save
-        render json: @blog_post
-    else
-        render json: {error: 'Error Creating Blog Post'}
+
+      def create
+          @blog_post = BlogPost.new(blog_post_params)
+          if @blog_post.save
+              render json: @blog_post
+          else
+              render json: {error: 'Error Creating Blog Post'}
+          end
+
+      end
+
+
+
+    def show
+        @blog_post = BlogPost.find(params[:id])
+        render json: BlogpostSerializer.new(@blog_post)
     end
 
-end
 
-def show
-    @blog_post = BlogPost.find(params[:id])
-    render json: @blog_post
-end
+      def update
+          @blog_post = BlogPost.find(params[:id])
 
-def destroy
-  @blog_post =  BlogPost.find(params[:id])
-  @blog_post.destroy
-end
+           @blog_post.update(title: params[:title], content: params[:content])
+          # binding.pry
+           @blog_post.save
+              render json: @blog_post
+          # else
+          #     render json: {error: @blog_post.error}
+          # end
 
-def update
-  @blog_post = BlogPost.find(params[:id])
 
-  @blog_post.update(title: params[:title], content: params[:content])
-  # binding.pry
-  if @blog_post.save
-    render json: @blog_post
-  else
-    render json: {error: @blog_post.error}
-  end
-end
+      end
 
 
 
+      private
 
-
-private
-
-  def blog_post_params
-      params.require(:blog_post).permit(:title, :content)
-  end
+      def blog_post_params
+          params.require(:blog_post).permit(:title, :content)
+      end
 end
